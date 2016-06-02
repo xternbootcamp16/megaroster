03c25be7bfebb4a48de0f3cd7e2c43ecac261eb5
Dave Strus <dave@getfretless.com>
2016-06-02 14:14:49 -0400

    Setup submit handler for the form.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation()
+
+var megaRoster = {
+  init: function() {
+    document.querySelector('form').onsubmit = this.addStudent;
+  },
+
+  addStudent: function(ev) {
+    ev.preventDefault();
+    var studentName = this.studentName.value;
+    console.log(studentName);
+  }
+};
+megaRoster.init();
