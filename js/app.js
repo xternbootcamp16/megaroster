178ace81777bd73dd823208685656bfa19d3daba
Dave Strus <dave@getfretless.com>
2016-06-03 14:58:38 -0400

    Edit student names.

--- js/app.js
+++ js/app.js
@@ -32,7 +32,10 @@ var megaRoster = {
 
   buildListItem: function(studentName) {
     var listItem = document.createElement('li');
-    listItem.innerText = studentName;
+    var span = document.createElement('span');
+    span.innerText = studentName;
+    span.className = 'studentName';
+    listItem.appendChild(span);
     this.appendLinks(listItem);
 
     return listItem;
@@ -67,6 +70,13 @@ var megaRoster = {
         this.moveDown(listItem);
       }.bind(this)
     });
+    span.appendChild(this.buildLink({
+      text: 'edit',
+      className: 'edit',
+      handler: function() {
+        this.toggleEditable(listItem.querySelector('span.studentName'));
+      }.bind(this)
+    }));
     span.appendChild(removeLink);
     span.appendChild(promoteLink);
     span.appendChild(moveUpLink);
@@ -81,6 +91,19 @@ var megaRoster = {
     link.onclick = options.handler;
     link.className += (options.className || '');
     return link;
+  },
+
+  toggleEditable: function(el) {
+    var toggleElement = el.parentElement.querySelector('a.edit');
+    if (el.contentEditable === 'true') {
+      el.contentEditable = 'false';
+      toggleElement.innerHTML = 'edit';
+    }
+    else {
+      el.contentEditable = 'true';
+      el.focus();
+      toggleElement.innerHTML = 'save';
+    }
   },
 
   promote: function(listItem) {
