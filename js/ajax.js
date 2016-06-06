bb270adda7fc1fac910a5283e72c92166a10ef43
Dave Strus <dave@getfretless.com>
2016-06-06 12:56:53 -0400

    Load mutants from API.

--- js/ajax.js
+++ js/ajax.js
@@ -15,6 +15,28 @@ $.extend(megaRoster, {
     }.bind(megaRoster));
   },
 
+  setupMutantAjax: function() {
+    $('a[data-remote-mutants="true"]').on('click', function(ev) {
+      ev.preventDefault();
+      $.ajax({
+        url: $(ev.currentTarget).attr('href'),
+        method: 'get',
+        success: function(data) {
+          this.loadMutants(data);
+        }.bind(this)
+      })
+    }.bind(megaRoster));
+  },
+
+  loadMutants: function(data) {
+    $.each(data, function(i, mutant) {
+      this.addStudent({
+        name: mutant.mutant_name + ' (' + mutant.power + ')',
+        id: mutant.id
+      });
+    }.bind(this));
+  },
+
   loadResults: function(data) {
     if (data.students) {
       $.each(data.students, function(index, student) {
--- js/megaroster.js
+++ js/megaroster.js
@@ -56,6 +56,7 @@ var megaRoster = {
     doc.on('submit', '.student form', this.saveStudent.bind(this));
 
     this.setupAjax();
+    this.setupMutantAjax();
   },
 
   addStudentViaForm: function(ev) {
